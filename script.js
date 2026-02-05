a08a2417ab6f00eeef2f1903a22647f92df23789
pablocanoburcher <pablocanoburcher@gmail.com>
2026-02-05 09:19:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const API_BASE_URL = '';
 
 // Wompi Configuration - Replace with your public key
 // Sandbox: pub_test_XXXXXXXX | Production: pub_prod_XXXXXXXX
-const WOMPI_PUBLIC_KEY = 'pub_test_qK2KWQOsW6WSr2IycRPwZcOeif1amNtl';
+const WOMPI_PUBLIC_KEY = 'pub_prod_3KbWCen3036LsV58opaQeANL52xPU9di';
 
 // Currency: COP for Colombian Pesos
 const WOMPI_CURRENCY = 'COP';
